d332837a14a30791af0a0a5fac99a0ffe3e4b002
stephen-softdev <stephensoftdev@gmail.com>
2025-11-24 19:06:18 +0530

    the conditional statement is commit by#stephen-softdev

---
+++ 04_1_conditional_statement.js
@@ -0,0 +1,28 @@
+var i = 215;
+if (i === 215) {
+  console.log("i is equal to the 215");
+}
+
+var age = 19;
+if (age <= 18) {
+  console.log("The person is adult age");
+} else {
+  console.log("The person is not a adult");
+}
+
+var n = "vanila";
+
+if (n === "vanila") {
+  console.log("IT is a vanila");
+} else if (n === "chocolate") {
+  console.log("It is a chocolate");
+} else {
+  console.log("This flavour");
+}
+
+var result =
+  n < 1
+    ? console.log("n is a negative number")
+    : console.log("n is a positive number");
+
+var cs = 10;
