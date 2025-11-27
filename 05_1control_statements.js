9bff6b7971a3985e79aa05a2b4adc0e00d682c19
stephen-softdev <stephensoftdev@gmail.com>
2025-11-27 08:57:33 +0530

    the control statements is completed by #stephensoftdev

--- 05_1control_statements.js
+++ 05_1control_statements.js
@@ -2,3 +2,14 @@ let i =1;
 for(i;i<10;i++){
     console.log("this is iteration ",i);
 }
+let j=1;
+while(j<10){
+    console.log("this is iteration",j);
+    j++;
+}
+let k =1;
+do{
+    console.log("this is do statement is executed",k,"time");
+    k++;
+}
+while(k<10);
