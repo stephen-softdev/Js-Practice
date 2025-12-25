b071b174a82ad8a87fe19e0b8553873632e074b3
stephen-softdev <stephensoftdev@gmail.com>
2025-12-25 19:11:21 +0530

    date  and time updated by #stephen-softdev

--- 13datetime.js
+++ 13datetime.js
@@ -11,11 +11,14 @@ const monthname = currentdate.toLocaleDateString("default", { month: "long" });
 const hour = currentdate.getHours();
 const minute = currentdate.getMinutes();
 const second = currentdate.getMinutes();
-
+let apm = "";
 console.log(" Current date:", date1, "-", month, "-", year);
 console.log("current time:", hour, ":", minute, ":", second);
 
 if (hour > 12) {
-  console.log("current time:", hour - 12, ":", minute, ":", second);
+  apm = "PM";
+  console.log("current time:", hour - 12, ":", minute, ":", second, " ", apm);
+} else {
+  apm = "AM";
+  console.log(" Current date:", date1, "-", monthname, "-", year, " ", apm);
 }
-console.log(" Current date:", date1, "-", monthname, "-", year);
